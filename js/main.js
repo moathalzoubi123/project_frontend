7d1a59691eb3186938336499fb6191be61fcef48
moath al-abdulrazaq <moath@moaths-air.home>
2022-08-23 17:22:44 +0300

    3rd

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const transactType = document.querySelector("#transact-type")
+
+
+const paymentCard = document.querySelector(".payment-card")
+
+const transferCard = document.querySelector(".transfer-card"); 
+
+const depositCard = document.querySelector(".deposit-card")
+
+transactType.addEventListener("change", ()=>{
+    switch(transactType.value){
+
+        case "payment": 
+        paymentCard.style.display = "block"
+        paymentCard.nextElementSibling.style.display = "none"
+        break; 
+
+
+        case "transfer": 
+        transferCard.previousElementSibling.style.display = "none"
+        transferCard.style.display = "block"
+        transferCard.nextElementSibling.style.display = "none"
+        break; 
+
+    }
+})
